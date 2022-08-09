abe5a7cae367b31ef611ffeef90f66c52038b1a4
Stephen Kurniawan <stepkurniawan@gmail.com>
2022-08-09 16:20:23 +0200

    move the df_journal dropna to data cleaning

--- split_csv.py
+++ split_csv.py
@@ -1,6 +1,12 @@
 import pandas as pd
+from pathlib import Path
 
-filename= "df_final"
-source_path = ".\\files2\\" + filename + ".csv"
+
+filename= "df_edges_full"
+source_path = "C:\\Users\\steph\\Desktop\\" + filename + ".csv"
+dest_path = '.\\files2\\'+ filename + '\\'+ filename + '_{}.csv'
+
+Path(".\\files2\\"+ filename).mkdir(parents=True, exist_ok=True)
+
 for i,chunk in enumerate(pd.read_csv(source_path, chunksize=40000)):
-    chunk.to_csv('.\\files\\'+ filename + '\\'+ filename + '_{}.csv'.format(i), index=False)
+    chunk.to_csv(dest_path.format(i), index=False)
